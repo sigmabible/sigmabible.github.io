634c01bb63999a32f08efd390fd2b86d281f8702
sigmabible <125644358+sigmabible@users.noreply.github.com>
2023-02-17 09:21:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+let slideIndex = 0;
+let timeoutId = null;
+const slides = document.getElementsByClassName("mySlides");
+const dots = document.getElementsByClassName("dot");
+
+showSlides();
+function currentSlide(index) {
+     slideIndex = index;
+     showSlides();
+}
+function plusSlides(step) {
+  
+  if(step < 0) {
+      slideIndex -= 2;
+      
+      if(slideIndex < 0) {
+        slideIndex = slides.length - 1;
+      }
+  }
+  
+  showSlides();
+}
+function showSlides() {
+  for(let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    dots[i].classList.remove('active');
+  }
+  slideIndex++;
+  if(slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].classList.add('active');
+   if(timeoutId) {
+      clearTimeout(timeoutId);
+   }
+  timeoutId = setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
